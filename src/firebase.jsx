76f77271d9948aace29d863f5c02d148a661d67e
notjawad404 <jawadali000123@gmail.com>
2024-01-18 20:03:46 +0500

    Initialized Firebase

---
+++ src/firebase.jsx
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseApp = firebase.initializeApp({
+apiKey: `${import.meta.env.VITE_apiKey}`,
+authDomain: `${import.meta.env.VITE_authDomain}`,
+projectId: `${import.meta.env.VITE_projectId}`,
+storageBucket: `${import.meta.env.VITE_storageBucket}`,
+messagingSenderId: `${import.meta.env.VITE_messagingSenderId}`,
+appId: `${import.meta.env.VITE_appId}`,
+measurementId: `${import.meta.env.VITE_measurementId}`,
+})
+
+export default firebaseApp;
