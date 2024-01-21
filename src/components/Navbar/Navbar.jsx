ec4145cb592578140f7ce623df99d9643a749282
notjawad404 <jawadali000123@gmail.com>
2024-01-21 22:29:03 +0500

    Added logout functionality in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
-
+import {   signOut } from "firebase/auth"
+import {  useNavigate } from "react-router-dom";
+import { useFirebase } from "../context/authContext";
 
 export default function Navbar() {
     const token = localStorage.getItem('token')
@@ -13,6 +15,9 @@ export default function Navbar() {
 }
 
 function PublicNavbar() {
+
+    
+
     return (
         <div className='bg-orange-400 text-slate-800 py-2 px-2'>
 
@@ -35,6 +40,19 @@ function PublicNavbar() {
 
 
 function PrivateNavbar() {
+    const auth = useFirebase().auth
+  const navigate = useNavigate()
+  const logout = async () =>{
+    try {
+      await signOut(auth);
+      localStorage.removeItem('token')
+      localStorage.removeItem('user')
+      navigate('/login')
+    } catch (error) {
+      alert("Error cannot logout")
+    } 
+  }
+
     return (
         <div className='bg-orange-400 text-slate-800 py-2 px-2'>
 
@@ -50,7 +68,7 @@ function PrivateNavbar() {
                     <Link className='mx-2 py-1' to='/addproject'>Add Project</Link>
                     <Link className='mx-2 py-1' to='/myprojects'>My Projects</Link>
                     <Link className='mx-2 py-1' to='/userprofile'>User Profile</Link>
-                    <Link className='p-1 bg-green-600 rounded-lg hover:bg-slate-100 hover:text-slate-800' to='/login' onClick={() => localStorage.removeItem('token')}>Logout</Link>
+                    <Link className='p-1 bg-green-600 rounded-lg hover:bg-slate-100 hover:text-slate-800' to='/login' onClick={logout}>Logout</Link>
                 </div>
             </div>
 
--- src/components/Public/home.jsx
+++ src/components/Public/home.jsx
@@ -1,29 +1,16 @@
-import {   signOut } from "firebase/auth"
-import {  useNavigate } from "react-router-dom";
-import { useFirebase } from "../context/authContext";
+
 import Navbar from "../Navbar/Navbar";
 
+import bg1 from '../../assets/Logo/bg1.png'
 
 export default function Home() {
-  const auth = useFirebase().auth
-  const navigate = useNavigate()
-  const logout = async () =>{
-    try {
-      await signOut(auth);
-      localStorage.removeItem('token')
-      localStorage.removeItem('user')
-      navigate('/login')
-    } catch (error) {
-      alert("Error cannot logout")
-    }
 
-    
-  }
   return (
     <div className="bg-slate-800 h-screen text-slate-50">
       <Navbar/>
-        <h1>Welcone to OC Hub</h1>
-        <button onClick={logout}>Logout</button>
+      <div>
+        <img className="" src={bg1} alt="OC Hub Logo"/>
+      </div>
     </div>
   )
 }
