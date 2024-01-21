9168aa3da58f279c5b205b9dd4ce7a8501bc363c
notjawad404 <jawadali000123@gmail.com>
2024-01-21 22:34:33 +0500

    Added banner to homepage

--- src/components/Public/home.jsx
+++ src/components/Public/home.jsx
@@ -6,10 +6,10 @@ import bg1 from '../../assets/Logo/bg1.png'
 export default function Home() {
 
   return (
-    <div className="bg-slate-800 h-screen text-slate-50">
+    <div className="bg-slate-800 h-screen overflow-y-auto text-slate-50">
       <Navbar/>
       <div>
-        <img className="" src={bg1} alt="OC Hub Logo"/>
+        <img className=" h-[500px] mx-auto" src={bg1} alt="OC Hub Logo"/>
       </div>
     </div>
   )
