ec9bc72b8fe8ebea1020e2c4f72ae52ed290fce3
notjawad404 <jawadali000123@gmail.com>
2024-01-22 17:32:26 +0500

    Added About page

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -1,9 +1,24 @@
-
+import aboutImg from '../../assets/Logo/whitebgBanner.png'
 
 export default function About() {
   return (
-    <div>
-About Us      
+    <div className="text-slate-100">
+
+        <div className='flex justify-center mx-5'>
+        <div className='w-1/2'>
+        <img src={aboutImg} alt='aboutImg' className='w-full'/>
+        </div>
+        <div className='w-1/2 px-4 my-auto'>
+        <div className='text-center text-lg'>
+        <p className="text-center text-2xl font-bold">{"<About us />"}</p><br></br>
+        <span>
+        At Collab Connect, we are committed to breaking down barriers and facilitating collaboration on a global scale. Our platform is built on the principles of openness, inclusivity, and creativity, providing a virtual space where individuals from diverse backgrounds can come together to work on projects that inspire them.
+
+        </span>
+            </div>
+
+        </div>  
+        </div>    
     </div>
   )
 }
--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -3,10 +3,10 @@ import bg1 from '../../assets/Logo/bg1.png'
 
 export default function Banner() {
   return (
-<div className="">
-        <img className=" h-[500px] mx-auto" src={bg1} alt="OC Hub Logo"/>
-        <h1 className="text-center text-2xl font-bold text-blue-900">
-        Unleashing Creativity, Connecting Minds, Building Futures
+<div className="py-5">
+        <img className=" h-[400px] mx-auto" src={bg1} alt="OC Hub Logo"/>
+        <h1 className="py-2 text-center text-2xl font-bold text-slate-100">
+            A Place Where Ideas Meet Collaboration
         </h1>
       </div>
   )
