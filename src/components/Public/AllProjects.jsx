9f78f49340ec5c870b8a3b3665aebb01fb85d511
notjawad404 <jawadali000123@gmail.com>
2024-01-23 17:09:23 +0500

    added All Projects Page

--- src/components/Public/AllProjects.jsx
+++ src/components/Public/AllProjects.jsx
@@ -1,29 +1,71 @@
+import { useState, useEffect } from "react";
 import Navbar from "../Navbar/Navbar";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
 
 export default function AllProjects() {
-  return (
-    <div className="bg-slate-800 h-screen">
-      <Navbar />
-      <div className="bg-slate-100 w-[500px] mx-auto mt-10 py-2">
-        <div className="flex flex-row justify-between px-2">
-          <h1>User Name</h1>
-          <h1>Paid/UnPaid</h1>
-        </div>
-        <div className="flex flex-row justify-between px-2">
-          <h1>Name</h1>
-          <h1>GitHub Link</h1>
-        </div>
-        <div className="flex flex-row px-2">
-          <h1 className="">Tech Stack: </h1>
-          <div className="px-2">
-          <h1 className="">Reactjs
-          </h1>
+  const [fetchData, setFetchData] = useState([]);
+
+  const dbref = collection(db, 'projects');
+
+  const fetch = async () => {
+    try {
+      const querySnapshot = await getDocs(dbref);
+      const fetchdata = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setFetchData(fetchdata);
+      console.log(fetchdata);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
+  const repoLink = (project) => {
+    window.open(project.RepoLink, '_blank');
+  };
+
+  const renderTechStack = (techStack) => {
+    return (
+      <div className="flex">
+        {techStack.split(',').map((stack, index) => (
+          <div key={index} className="bg-red-400 mx-1 inline-block rounded-lg whitespace-nowrap">
+            <span className="px-2">{stack.trim()}</span>
           </div>
-        </div>
-        <div className="flex justify-center">
-          <button className="bg-red-400 p-1">Project Details</button>
-        </div>
+        ))}
+      </div>
+    );
+  };
+  
+
+  return (
+    <div className="bg-slate-800 h-screen overflow-y-auto">
+      <Navbar />
+      <div>
+        {fetchData.map((project, index) => (
+          <div key={index} className="bg-slate-100 w-[500px] mx-auto mt-10 py-2">
+            <div className="flex flex-row justify-between px-2">
+              <h1>{project.ProjectName}</h1>
+              <h1>{project.PaidProject}</h1>
+            </div>
+            <div className="flex flex-row justify-between p-2">
+              <button onClick={() => repoLink(project)} className="bg-blue-600 p-1 rounded-lg text-slate-100 cursor-pointer">GitHub Repo</button>
+              <h1></h1>
+            </div>
+            <div className="flex flex-row px-2 pb-2">
+              <h1 className="whitespace-nowrap pr-2">Tech Stack: </h1>
+              <div className="flex px-2 py-2 overflow-x-auto bg-slate-500">
+                {renderTechStack(project.TechStack)}
+              </div>
+            </div>
+            <div className="flex justify-center">
+              <button className="bg-red-400 p-1">Project Details</button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
