1025c9431d6a306bacfed293d721b25506335207
notjawad404 <jawadali000123@gmail.com>
2024-01-23 17:47:34 +0500

    Added My Projects

--- src/components/projectManagment/MyProjectsList.jsx
+++ src/components/projectManagment/MyProjectsList.jsx
@@ -1,9 +1,81 @@
-
+import { useState, useEffect } from "react";
+import Navbar from "../Navbar/Navbar";
+import { query, where, collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
 
 export default function MyProjectsList() {
+  const [fetchData, setFetchData] = useState([]);
+
+  const userID = localStorage.getItem('token');
+  // const dbref = collection(db, 'projects');
+
+  const fetch = async () => {
+    try {
+      const querySnapshot = await getDocs(query(collection(db, 'projects'), where('UserToken', '==', userID)));
+      const fetchdata = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setFetchData(fetchdata);
+      console.log(fetchdata);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
+  const repoLink = (project) => {
+    window.open(project.RepoLink, '_blank');
+  };
+
+  const renderTechStack = (techStack) => {
+    return (
+      <div className="flex">
+        {techStack.split(',').map((stack, index) => (
+          <div key={index} className="bg-red-400 mx-1 inline-block rounded-lg whitespace-nowrap">
+            <span className="px-2">{stack.trim()}</span>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
+
   return (
-    <div>
-      ProjectsList
+    <div className="bg-slate-800 h-screen overflow-y-auto">
+      <Navbar />
+      <div>
+        {fetchData.map((project, index) => (
+          <div key={index} className="bg-slate-100 w-[500px] mx-auto mt-10 py-2">
+            <div className="flex flex-row justify-between px-2">
+              <h1>{project.ProjectName}</h1>
+              <h1>{project.PaidProject}</h1>
+            </div>
+            <div className="flex flex-row justify-between p-2">
+              <button onClick={() => repoLink(project)} className="bg-blue-600 p-1 rounded-lg text-slate-100 cursor-pointer">GitHub Repo</button>
+              <h1></h1>
+            </div>
+            <div className="flex flex-row px-2 pb-2">
+              <h1 className="whitespace-nowrap pr-2">Tech Stack: </h1>
+              <div className="flex px-2 py-2 overflow-x-auto bg-slate-500">
+                {renderTechStack(project.TechStack)}
+              </div>
+            </div>
+            <div className="flex justify-between">
+            <div>
+            </div>
+            <div>
+            </div>
+              <button className="bg-red-400 p-1 mx-1 rounded-lg">Project Details</button>
+              <button className="bg-red-400 p-1 mx-1 rounded-lg">Update</button>
+              <button className="bg-red-400 p-1 mx-1 rounded-lg">Delete</button>
+
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
+
